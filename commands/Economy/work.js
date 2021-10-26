ef2aefaa2524f78ea3aa81eeeb38b05127a01af0
Yoshiboi18303 <yoshiboi18303.t@gmail.com>
2021-10-26 16:38:06 +0000

    Added first voting reward!

--- commands/Economy/work.js
+++ commands/Economy/work.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Profiles = require('../../schemas/profileSchema');
 const colors = require('../../colors.json');
+const Users = require('../../schemas/userSchema');
 
 var jobs = [
   "Bot Developer",
@@ -54,22 +55,87 @@ module.exports = {
             .setDescription(`You worked as a ${random_job} and earned ${random_coins} coins out of it!`)
           await interaction.reply({ embeds: [success_embed], ephemeral: true })
         } else { */
-          var random_coins = Math.floor(Math.random() * 250)
-          var random_job = jobs[Math.floor(Math.random() * jobs.length)]
-          data = await Profiles.findOneAndUpdate({
-            id: interaction.user.id
-          },
-          {
-            $inc: {
-              coins: random_coins
+        Users.findOne({ id: interaction.user.id }, async (err, data) => {
+          if(err) throw err
+          if(!data) {
+            data = new Users({
+              id: interaction.user.id
+            })
+            data.save()
+            if(data.voted) {
+              var random_coins = Math.floor(Math.random() * 500)
+              var random_job = jobs[Math.floor(Math.random() * jobs.length)]
+              data = await Profiles.findOneAndUpdate({
+                id: interaction.user.id
+              },
+              {
+                $inc: {
+                  coins: random_coins
+                }
+              })
+              data.save()
+              const success_embed = new MessageEmbed()
+                .setColor(colors.green)
+                .setTitle("Success")
+                .setDescription(`You worked as a ${random_job} and earned ${random_coins} coins out of it!`)
+              await interaction.reply({ embeds: [success_embed], ephemeral: true })
+            } else {
+              var random_coins = Math.floor(Math.random() * 250)
+              var random_job = jobs[Math.floor(Math.random() * jobs.length)]
+              data = await Profiles.findOneAndUpdate({
+                id: interaction.user.id
+              },
+              {
+                $inc: {
+                  coins: random_coins
+                }
+              })
+              data.save()
+              const success_embed = new MessageEmbed()
+                .setColor(colors.green)
+                .setTitle("Success")
+                .setDescription(`You worked as a ${random_job} and earned ${random_coins} coins out of it!`)
+              await interaction.reply({ embeds: [success_embed], ephemeral: true })
             }
-          })
-          data.save()
-          const success_embed = new MessageEmbed()
-            .setColor(colors.green)
-            .setTitle("Success")
-            .setDescription(`You worked as a ${random_job} and earned ${random_coins} coins out of it!`)
-          await interaction.reply({ embeds: [success_embed], ephemeral: true })
+          } else {
+            console.log(data.voted)
+            if(data.voted) {
+              var random_coins = Math.floor(Math.random() * 375)
+              var random_job = jobs[Math.floor(Math.random() * jobs.length)]
+              data = await Profiles.findOneAndUpdate({
+                id: interaction.user.id
+              },
+              {
+                $inc: {
+                  coins: random_coins
+                }
+              })
+              data.save()
+              const success_embed = new MessageEmbed()
+                .setColor(colors.green)
+                .setTitle("Success")
+                .setDescription(`You worked as a ${random_job} and earned ${random_coins} coins out of it!`)
+              await interaction.reply({ embeds: [success_embed], ephemeral: true })
+            } else {
+              var random_coins = Math.floor(Math.random() * 250)
+              var random_job = jobs[Math.floor(Math.random() * jobs.length)]
+              data = await Profiles.findOneAndUpdate({
+                id: interaction.user.id
+              },
+              {
+                $inc: {
+                  coins: random_coins
+                }
+              })
+              data.save()
+              const success_embed = new MessageEmbed()
+                .setColor(colors.green)
+                .setTitle("Success")
+                .setDescription(`You worked as a ${random_job} and earned ${random_coins} coins out of it!`)
+              await interaction.reply({ embeds: [success_embed], ephemeral: true })
+            }
+          }
+        })
         // }
       }
     })
